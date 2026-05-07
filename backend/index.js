a2e0ae274789a09f88af1a3a133e75b5ef774f48
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-05-07 17:41:25 +0100

    fix CORS and backend updates

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS (dev setup)
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 //  ROUTES 
 app.use("/api/v1/user", userRoute);
