4223417a8b51b88917c1544760ecc3a173644c11
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-05-13 14:42:51 +0100

    update

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -120,18 +120,18 @@ export const login = async (req, res) => {
     };
 
     return res
-      .status(200)
-      .cookie("token", token, {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production"
-      })
-      .json({
-        message: `Welcome back ${user.fullname}`,
-        user,
-        success: true
-      });
+  .cookie("token", token, {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true
+  })
+  .status(200)
+  .json({
+    message: `Welcome back ${user.fullname}`,
+    user,
+    success: true
+  });
 
   } catch (error) {
     console.log(error);
