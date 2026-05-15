6688d56f76f654319619f549b1e99df970485339
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-05-15 16:18:05 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 /* -------------------- CORS CONFIG -------------------- */
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
