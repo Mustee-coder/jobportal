efc3067238a2635b70f79d0317965228a7f7d455
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-08-10 16:27:32 +0100

    fix frontend job posting request

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -9,3 +9,23 @@ const connectDB = async () => {
     }
 }
 export default connectDB;
+
+
+
+MONGO_URI=mongodb+srv://magajimujittapha5_db_user:ItdXYyYs1l7sr1mb@cluster0.v4xmzzw.mongodb.net/?appName=Cluster0
+
+PORT=3000
+
+SECRET_KEY=jsgekgwogdlgeuosgdogeyoxhdyk
+
+
+CLOUD_NAME=dppzcdfxi 
+
+API_KEY=463599621661158
+
+API_SECRET=uk5rtZUsG98a4UqoWnKIQ0l-ZrY
+
+
+
+ 
+ 
