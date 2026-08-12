d9d8f83d97273b7c474327b420f13c6cb4ad0d24
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-08-12 23:06:07 +0100

    Replace vulnerable datauri dependency

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -1,11 +1,12 @@
-import DataUriParser from "datauri/parser.js"
-
 import path from "path";
 
 const getDataUri = (file) => {
-    const parser = new DataUriParser();
-    const extName = path.extname(file.originalname).toString();
-    return parser.format(extName, file.buffer);
-}
+  const extName = path.extname(file.originalname);
+
+  return {
+    content: `data:${file.mimetype};base64,${file.buffer.toString("base64")}`,
+    name: `${path.basename(file.originalname, extName)}${extName}`,
+  };
+};
 
 export default getDataUri;
